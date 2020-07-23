9f0423d55db149115d01f0100d999d710103df5a
Hanhong Xue <macroxue@hotmail.com>
2020-07-22 22:41:03 -0400

    再次修复单字输入时体示框的闪烁问题

--- install_rime.sh
+++ install_rime.sh
@@ -18,10 +18,32 @@ max_phrase_length: 2
 columns:
   - text
   - code
+  - weight
 ...
 
+b	b	1
+p	p	1
+m	m	1
+f	f	1
+d	d	1
+t	t	1
+l	l	1
+n	n	1
+g	g	1
+k	k	1
+h	h	1
+j	j	1
+q	q	1
+x	x	1
+r	r	1
+z	z	1
+c	c	1
+s	s	1
+y	y	1
+w	w	1
 END
-sed -e "s/\(.*\)	\(.*\)	[1-9][0-9]*$/\2	\1/" code.txt | sed -e "s/  *//" >> $dict
+sed -e "s/\(.*\)	\(.*\)	[1-9][0-9]*$/\2	\1/" code.txt | sed -e "s/  *//" | \
+  sed -e "s/$/	100/" >> $dict
 
 # 拷贝配置和字典
 mkdir -p ~/.config/fcitx/rime
